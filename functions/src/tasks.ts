4164b3a610416507f7b7dd4de5cfcb41822b2a05
Schwinn Saereesitthipitak <winaichan263@gmail.com>
2019-11-08 23:11:56 +0700

    Fixed tasks legacy

--- functions/src/tasks.ts
+++ functions/src/tasks.ts
@@ -27,72 +27,73 @@ export const getAllTasks = functions
 
 export const getTasksWithFilter = functions
 	.region("asia-east2")
-	.https.onRequest(async (req, res) => {
-		res.set("Access-Control-Allow-Origin", "*");
-		const limit = req.query.limit;
-		const min_difficulty = req.query.min_difficulty;
-		const max_difficulty = req.query.max_difficulty;
-		const tags = req.query.tags;
+	.https.onCall(
+		async (request_data: any, context: functions.https.CallableContext) => {
+			const limit = request_data.query.limit;
+			const min_difficulty = request_data.query.min_difficulty;
+			const max_difficulty = request_data.query.max_difficulty;
+			const tags = request_data.query.tags;
 
-		if (!(typeof limit === "number") || limit <= 0) {
-			throw new functions.https.HttpsError(
-				"invalid-argument",
-				"Limit must be a number > 0"
-			);
-		}
+			if (!(typeof limit === "number") || limit <= 0) {
+				throw new functions.https.HttpsError(
+					"invalid-argument",
+					"Limit must be a number > 0"
+				);
+			}
 
-		if (!(typeof min_difficulty === "number") || min_difficulty < 0) {
-			throw new functions.https.HttpsError(
-				"invalid-argument",
-				"Min difficulty must be a number >= 0"
-			);
-		}
+			if (!(typeof min_difficulty === "number") || min_difficulty < 0) {
+				throw new functions.https.HttpsError(
+					"invalid-argument",
+					"Min difficulty must be a number >= 0"
+				);
+			}
 
-		if (
-			!(typeof max_difficulty === "number") ||
-			max_difficulty < min_difficulty
-		) {
-			throw new functions.https.HttpsError(
-				"invalid-argument",
-				"Max difficulty must be a number >= Min difficulty"
-			);
-		}
+			if (
+				!(typeof max_difficulty === "number") ||
+				max_difficulty < min_difficulty
+			) {
+				throw new functions.https.HttpsError(
+					"invalid-argument",
+					"Max difficulty must be a number >= Min difficulty"
+				);
+			}
 
-		if (!(tags instanceof Array)) {
-			throw new functions.https.HttpsError(
-				"invalid-argument",
-				"Tags must be a string[]"
-			);
-		}
+			if (!(tags instanceof Array)) {
+				throw new functions.https.HttpsError(
+					"invalid-argument",
+					"Tags must be a string[]"
+				);
+			}
 
-		try {
-			const taskDocRefs = admin
-				.firestore()
-				.collection("tasks")
-				.orderBy("difficulty")
-				.limit(limit)
-				.where("difficulty", ">=", min_difficulty)
-				.where("difficulty", "<=", max_difficulty);
-			const taskDocs = await taskDocRefs.get();
-			const result: Object[] = [];
-			taskDocs.docs.forEach(doc => {
-				const data = doc.data();
-				let valid = true;
-				tags.forEach((tag: String) => {
-					if (!data.tags.includes(tag)) {
-						valid = false;
+			try {
+				const taskDocRefs = admin
+					.firestore()
+					.collection("tasks")
+					.orderBy("difficulty")
+					.limit(limit)
+					.where("difficulty", ">=", min_difficulty)
+					.where("difficulty", "<=", max_difficulty);
+				const taskDocs = await taskDocRefs.get();
+				const result: Object[] = [];
+				taskDocs.docs.forEach(doc => {
+					const data = doc.data();
+					let valid = true;
+					tags.forEach((tag: String) => {
+						if (!data.tags.includes(tag)) {
+							valid = false;
+						}
+					});
+					if (valid) {
+						result.push(data);
 					}
 				});
-				if (valid) {
-					result.push(data);
-				}
-			});
 
-			res.send(result);
-		} catch (error) {
-			throw new functions.https.HttpsError("unknown", error);
+				return result;
+			} catch (error) {
+				throw new functions.https.HttpsError("unknown", error);
+			}
 		}
-	});
+	);
 
 export const getProblemMetadata = functions
 	.region("asia-east2")
