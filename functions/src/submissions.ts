c8026a04b2ea58918cfd045cb5804100ecbff480
win11905 <donothavemoney@gmail.com>
2020-04-26 16:55:10 +0700

    Complete makeSubmission

--- functions/src/submissions.ts
+++ functions/src/submissions.ts
@@ -1,23 +1,27 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { writeCode } from "./util";
+import { unzipCode, writeCode } from "./util";
+import { checkAdmin } from "./admin";
 
 export const makeSubmission = functions
 	.region("asia-east2")
 	.https.onCall(
 		async (request_data: any, context: functions.https.CallableContext) => {
-			const { id, code, lang } = request_data;
+			let { id, code, lang } = request_data;
 			const uid = context.auth?.uid;
+			if (typeof code === "string") {
+				code = await unzipCode(code, id);
+			}
 			if (!(typeof id === "string") || id.length === 0) {
 				throw new functions.https.HttpsError(
 					"invalid-argument",
 					"Problem ID must be a non-empty string"
 				);
 			}
-			if (!Array.isArray(code)) {
+			if (!(Array.isArray(code) || typeof code === "string")) {
 				throw new functions.https.HttpsError(
 					"invalid-argument",
-					"Code must be a non-empty string"
+					"Code must be a non-empty Array of string"
 				);
 			}
 			if (!(typeof lang === "string") || lang.length === 0) {
@@ -32,23 +36,37 @@ export const makeSubmission = functions
 					.collection("tasks")
 					.where("id", "==", id)
 					.get();
-				if (taskSnapshot.docs.length === 0)
-					throw new Error("Not found problem!!!");
-				else if (taskSnapshot.docs.length === 1) {
-					const data = taskSnapshot.docs[0].data();
-					const submissionID = (
-						await admin.firestore().collection("submissions").add({
-							language: lang,
-							problem: data,
-							points: -1,
-							timestamp: admin.firestore.Timestamp.now(),
-							uid: uid,
-						})
-					).id;
-					await writeCode(submissionID, code);
-					return submissionID;
+				if (taskSnapshot.docs.length === 1) {
+					const taskDoc = taskSnapshot.docs[0].data();
+					if (taskDoc.visible === true || checkAdmin(context)) {
+						if (typeof code === "string") {
+							code = await unzipCode(code, taskDoc.fileName);
+							if (!Array.isArray(code)) {
+								throw new functions.https.HttpsError(
+									"invalid-argument",
+									"code must be zip file"
+								);
+							}
+						}
+						const submissionID = (
+							await admin
+								.firestore()
+								.collection("submissions")
+								.add({
+									language: lang,
+									task: taskDoc,
+									points: -1,
+									timestamp: admin.firestore.Timestamp.now(),
+									uid: uid,
+								})
+						).id;
+						await writeCode(submissionID, code);
+						return submissionID;
+					} else {
+						throw new Error("Permission denied");
+					}
 				} else {
-					throw new Error("Duplicate snapshots found!!!");
+					throw new Error("Have Problem with task!!!");
 				}
 			} catch (error) {
 				throw new functions.https.HttpsError("unknown", error);
