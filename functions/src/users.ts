b77497f77c54d2912cbee8bd9af38fc0aa345b6a
win11905 <donothavemoney@gmail.com>
2020-05-01 02:08:49 +0700

    Add getUserContext

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -1,6 +1,12 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
+const initialUser = {
+  displayName: '',
+  admin: false,
+  codeTheme: 'material',
+}
+
 export const onRegister = functions
   .region('asia-east2')
   .auth.user()
@@ -9,13 +15,36 @@ export const onRegister = functions
       try {
         const uid = user.uid
         const data = {
+          ...initialUser,
           displayName: user.displayName,
-          admin: false,
         }
         await admin.firestore().doc(`users/${uid}`).set(data)
       } catch (error) {
-        console.log('Error adding registration of user to database')
-        console.log(error)
+        throw new functions.https.HttpsError('unknown', error)
+      }
+    }
+  )
+
+export const getUserContext = functions
+  .region('asia-east2')
+  .https.onCall(
+    async (
+      request_data: functions.https.Request,
+      context: functions.https.CallableContext
+    ) => {
+      if (!context.auth) return initialUser
+      try {
+        const uid = context.auth.uid
+        const userSnapshot = await admin.firestore().doc(`users/${uid}`).get()
+        if (!userSnapshot.exists) {
+          throw new functions.https.HttpsError(
+            'data-loss',
+            'user not found in database'
+          )
+        }
+        return userSnapshot.data()
+      } catch (error) {
+        throw new functions.https.HttpsError('unknown', error)
       }
     }
   )
