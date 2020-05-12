bad3c768d3ba569964e16b65d860347bbdb2aec8
win11905 <donothavemoney@gmail.com>
2020-05-13 04:41:47 +0700

    Remove getUserContext

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -20,27 +20,3 @@ export const onRegister = functions
       }
     }
   )
-
-export const getUserContext = functions
-  .region('asia-east2')
-  .https.onCall(
-    async (
-      requestData: functions.https.Request,
-      context: functions.https.CallableContext
-    ) => {
-      if (!context.auth) return initialUser
-      try {
-        const uid = context.auth.uid
-        const userDoc = await admin.firestore().doc(`users/${uid}`).get()
-        if (!userDoc.exists) {
-          throw new functions.https.HttpsError(
-            'data-loss',
-            'user not found in database'
-          )
-        }
-        return userDoc.data()
-      } catch (error) {
-        throw new functions.https.HttpsError('unknown', error)
-      }
-    }
-  )
