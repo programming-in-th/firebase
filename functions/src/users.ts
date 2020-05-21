c51253ea91307b6e4dcaaff9a41838868db32ec8
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2020-05-21 19:29:58 +0700

    Add setUsername (#4)

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -44,3 +44,29 @@ export const getUserContext = functions
       }
     }
   )
+
+export const setUsername = functions
+  .region('asia-east2')
+  .https.onCall(
+    async (requestData: any, context: functions.https.CallableContext) => {
+      if (!context.auth) return {}
+      try {
+        const newuser = requestData.username
+        const uid = context.auth.uid
+        const userDocs = await admin
+          .firestore()
+          .collection('users')
+          .where('username', '==', newuser)
+          .get()
+        if (userDocs.docs.length === 0) {
+          await admin
+            .firestore()
+            .doc(`users/${uid}`)
+            .update({ username: newuser })
+          return true
+        } else return false
+      } catch (error) {
+        throw new functions.https.HttpsError('unknown', error)
+      }
+    }
+  )
