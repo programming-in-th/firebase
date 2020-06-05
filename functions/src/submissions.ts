b187120925247ad1486244dc731253ced1163721
Pongsaphol Pongsawakul <30414878+win11905@users.noreply.github.com>
2020-06-06 01:11:14 +0700

    Revert "Make code compatible with realtime submission (#18)" (#19)
    This reverts commit 4787b7df85bd9afc5c5922d50d01e1c48f3f055c.

--- functions/src/submissions.ts
+++ functions/src/submissions.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { unzipCode, readCode, writeCode, isAdmin } from './util'
-import * as crypto from 'crypto'
 
 export const makeSubmission = functions
   .region('asia-east2')
@@ -141,6 +140,7 @@ export const getSubmission = functions
         }
 
         return {
+          ...submission,
           username: user.username,
           task,
           humanTimestamp,
@@ -177,19 +177,10 @@ export const getSubmissions = functions
           }
 
           if (userDocs.docs.length !== 1) {
-            const tmp: string[] = []
-            for (const data of userDocs.docs) {
-              tmp.push(data.id)
-            }
-            for (const data of tmp) {
-              const tmpID = crypto.randomBytes(20).toString('hex')
-              await admin
-                .firestore()
-                .doc(`users/${data}`)
-                .update({ username: tmpID })
-            }
-            res.send({ results: [], next: null })
-            return
+            throw new functions.https.HttpsError(
+              'aborted',
+              'User fetching error'
+            )
           }
 
           const uid = userDocs.docs[0].id
